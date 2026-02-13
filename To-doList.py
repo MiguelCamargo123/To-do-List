d50d7f74f8679b1ad35e4dc79494e0a9ffdbf478
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-13 10:58:11 -0300

    Loop terminado e as funções de listar, adicionar e remover terminadas

--- To-doList.py
+++ To-doList.py
@@ -1,5 +1,3 @@
-import json
-
 print('============= Lista de tarefas ===============')
 
 lista = []
@@ -7,7 +5,24 @@ lista = []
 perguntaSairEntrar = input('Você quer entrar ou sair? (S/N) ').lower().upper()
 
 while perguntaSairEntrar == 'S':
+    print()
     print('O que você deseja fazer?')
     print()
     perguntaFazer = input('[A]dicionar ======= [L]istar ========== [R]emover ').lower().upper()
+    print()
+
+    if perguntaFazer == 'A':
+        adicionar  = input('Digite o que deseja adicionar: ')
+        lista.append(adicionar)
+        print(f'A tarefa {adicionar} foi adicionada com sucesso')
+
+    if perguntaFazer == 'L':
+        for i in lista:
+            print('Até o momento você adicionou as seguintes tarefas:')
+            print(i)
+            
+    if perguntaFazer == 'R':
+        remover = input('Digite o que deseja remover: ')
+        if remover in lista:
+            lista.pop(remover)
 
